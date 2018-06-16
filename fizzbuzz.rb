07701185c259635e327f6101eb80e3f1261a5052
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-16 16:08:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -5,7 +5,7 @@ def fizzbuzz(int)
     int % 3 == 0 && int % 5 != 0
     "Fizz"
   elsif
-    int % 5 == 0 && int % 5 != 0
+    int % 5 == 0 && int % 3 != 0
     "Buzz"
   else
     int % 5 == 0 && int % 3 == 0

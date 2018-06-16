94958d492b958982040ae0e3b6e8895932b2e93b
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-16 16:05:11 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,13 +2,13 @@
 # See README.md for instructions on how to do this
 def fizzbuzz
   if
-    int / 3 == 0
+    int % 3 == 0
     "Fizz"
   elsif
-    int / 5 == 0
+    int % 5 == 0
     "Buzz"
   else
-    int / 5 == 0 && int / 3 == 0
+    int % 5 == 0 && int % 3 == 0
     "FizzBuzz"
   end
 end
